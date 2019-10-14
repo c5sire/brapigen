e06e448080dfee424ed39e9c82bace7bfe213911
mverouden <maikel.verouden@gmail.com>
2019-10-14 15:32:41 +0200

    updates

--- R/generate.R
+++ R/generate.R
@@ -19,18 +19,22 @@ base::unlink(x = dir_b, recursive = TRUE, force = TRUE)
 base::list.files(path = dir_b, recursive = TRUE)
 
 base::dir.create(path = dir_b)
-dir_r <- base::file.path(dir_b, "R")
+dir_r <- base::file.path(dir_b, "R/")
 base::dir.create(path = dir_r)
 
 ### copy files
-base::file.copy(from = "inst/templates/zzz.R",
-                to = file.path(dir_r, "zzz.R"))
+fileNames <- base::setdiff(x = base::list.files("inst/templates/"),
+                           y = base::list.files("inst/templates/")[grepl(pattern = "*.mst",
+                                                                         x = base::list.files("inst/templates/"))])
+base::file.copy(from = paste0("inst/templates/", fileNames), to = paste0(dir_r, fileNames), overwrite = TRUE)
 
 ### Descriptionlib
+### function to retrieve all call names
 fetchCallNames <- function(brapiSpecs) {
   brapiSpecs[["paths"]] %>% names
 }
 
+### retrieve all call names in a readable format
 allCallNames <- fetchCallNames(brapiSpecs = brapiSpecs)
 allCallNames <- sub(pattern = "_",
                     replacement = "",
@@ -41,18 +45,18 @@ allCallNames <- sub(pattern = "_",
                       pattern = stringr::regex("\\{|\\}"),
                       replacement = ""))
 
-### Prior to 2019-10-11
-# getCall <- function(brapiSpecs, idName) {
-#   brapiCallNames <- fetchCallNames(brapiSpecs = brapiSpecs)
-#   ## usethis::use_package(package = "stringr")
-#   callName <- brapiCallNames[stringr::str_detect(string = brapiCallNames,
-#                                                  pattern = idName)]
-#   ## check type of the call and deprecation
-#   brapiSpecs[["paths"]][[callName]][["get"]]
-# }
+### only during development TO BE DELETED LATER
+rm(list = setdiff(objects(),
+                  c("brapiSpecs",
+                    "allCallNames",
+                    "dir_b",
+                    "dir_r",
+                    "fileNames",
+                    "fetchCallNames")))
 
+### function to retrieve call specifications for a GET call
 ### intended to loop over allCallNames, where each element of the vector will be
-### used idName
+### used as idName
 getCall <- function(brapiSpecs, idName) {
   brapiCallNames <- fetchCallNames(brapiSpecs = brapiSpecs)
   allCallNames <- sub(pattern = "_",
@@ -69,8 +73,9 @@ getCall <- function(brapiSpecs, idName) {
   if ("get" %in% names(brapiSpecs[["paths"]][[callName]])) {
     if (!("Deprecated" %in% brapiSpecs[["paths"]][[callName]][["get"]][["tags"]])) {
       aCall <- brapiSpecs[["paths"]][[callName]][["get"]]
-      aCall$verb <- "get"
-      aCall$name <- allCallNames[idNumber]
+      aCall[["name"]] <- allCallNames[idNumber]
+      aCall[["call"]] <- callName
+      aCall[["verb"]] <- "get"
       return(aCall)
     }
   } else {
@@ -78,81 +83,133 @@ getCall <- function(brapiSpecs, idName) {
   }
 }
 
-aCall <- getCall(brapiSpecs = brapiSpecs, idName = "commoncropnames")
-
+### Create aCall object containing call elements
+### tested on:
+### * calls
+### * commoncropnames
+### * markers
+### * germplasm_germplasmDbId_attributes
+aCall <- getCall(brapiSpecs = brapiSpecs, idName = "search_observationtables_searchResultsDbId")
+### Create aCallDesc object containing call description
 aCallDesc <- stringr::str_replace_all(string = stringr::str_replace_all(string = aCall[["description"]],
                                                            pattern =  c("\\n\\n\\n"),
                                                            replacement =  "\\\n\\\n"),
                          pattern =  c("\\n\\n"),
                          replacement =  "\n#' ")
 
+### function to generate @param section in the documentation
 aCallParamVector <- function(aCall) {
   n <- length(aCall$parameters)
   res <- character(n)
   for (i in 1:n) {
     p <- aCall$parameters[[i]]
-    res[i] <- paste0(p$name, " ",
-                     p$type, "; required: ",
-                     p$required, "; ",
-                     p$description)
+    if ("deprecated" %in% names(p)) {
+      res[i] <- paste0(p[["name"]], "; ",
+                       p[["description"]])
+    } else {
+      res[i] <- paste0(p$name, " ",
+                       ifelse(("items" %in% names(p[["schema"]])),
+                              "vector of type character",
+                              ifelse(p[["schema"]][["type"]] == "integer",
+                                     "integer",
+                                     "character")),
+                       "; required: ",
+                       p$required, "; ",
+                       p$description)
+    }
   }
   return(res)
 }
 
-aCallParams <- aCallParamVector(aCall = aCall)
+### Create @param description for documentation
+aCallParam <- aCallParamVector(aCall = aCall)
+aCallParam <- whisker::iteratelist(x = aCallParam, value = "pname")
+aCallParam <- lapply(X = aCallParam,
+                     FUN = function(el) {
+                       lapply(X = el, FUN = function(elel) {
+                         stringr::str_replace_all(string = elel,
+                                                  pattern = "\\n\\n",
+                                                  replacement = "\n#' ")
+                       })
+                     })
 
-aCallParams <- whisker::iteratelist(x = aCallParams, value = "pname")
-
-aCallParams <- lapply(X = aCallParams,
-                      FUN = function(el) {
-                        lapply(X = el, FUN = function(elel) {
-                          stringr::str_replace_all(string = elel,
-                                                   pattern = "\\n\\n",
-                                                   replacement = "\n#' ")
-                        })
-                      })
-
+### Create function for function arguments
 aCallParamString <- function(aCall) {
-  n <- length(aCall$parameters)
-  res <- character(n)
+  n <- length(aCall[["parameters"]])
+  res <- character(0)
   for (i in 1:n) {
-    if ("deprecated" %in% names(aCall$parameters[[i]])) {
+    if (aCall[["parameters"]][[i]][["name"]] == "Authorization" | "deprecated" %in% names(aCall[["parameters"]][[i]])) {
       next()
     } else {
       if (aCall[["parameters"]][[i]][["name"]] == "page" | aCall[["parameters"]][[i]][["name"]] == "pageSize") {
-        p <- aCall[["parameters"]][[i]][["name"]]
-        res[i] <- paste(aCall[["parameters"]][[i]][["name"]], "=", as.integer(aCall[["parameters"]][[i]][["example"]]))
+        res <- paste(res,
+                     paste(aCall[["parameters"]][[i]][["name"]],
+                           "=",
+                           as.integer(aCall[["parameters"]][[i]][["example"]])),
+                     sep = ", ")
       } else {
-        p <- aCall[["parameters"]][[i]][["name"]]
-        res[i] <- paste(aCall[["parameters"]][[i]][["name"]], "=", "''")
+        res <- paste(res,
+                     paste(aCall[["parameters"]][[i]][["name"]], "=", "''"),
+                     sep = ", ")
       }
     }
   }
-  res <- paste(res, collapse = ", ")
   res <- sub(pattern = "^, ",
              replacement = "",
              x = res)
   return(res)
 }
 
+### Creation function arguments for selected call
 aCallArgs <- aCallParamString(aCall = aCall)
 
+### Create function to identify required arguments
+aCallReqArgs <- function(aCall) {
+  n <- length(aCall[["parameters"]])
+  required <- character(0)
+  for (i in 1:n) {
+    p <- aCall$parameters[[i]]
+    if (p[["required"]] == TRUE) {
+      required <- paste(required,
+                        aCall[["parameters"]][[i]][["name"]],
+                        sep = ", ")
+    } else {
+      next()
+    }
+  }
+  required <- sub(pattern = "^, ",
+                  replacement = "",
+                  x = required)
+  if (length(required) == 0) {
+    aCall$required <- ""
+    return(aCall)
+  } else {
+    aCall$required <- required
+    return(aCall)
+  }
+}
+
+### Identify and store required arguments
+aCall <- aCallReqArgs(aCall = aCall)
+
+### Store call family information for documentation
 aCallFamily <- c(
   paste0("brapi_", brapiSpecs[["info"]][["version"]]),
   aCall$tags
 )
-
 aCallFamily <- whisker::iteratelist(aCallFamily, value = "fname")
 
-aCallData <- list(name = aCall$name,
-                  summary = aCall$summary,
-                  parameters = aCallParams,
+### Create call data for the selected call
+aCallData <- list(name = aCall[["name"]],
+                  summary = aCall[["summary"]],
+                  parameters = aCallParam,
                   description = aCallDesc,
                   family = aCallFamily,
                   arguments = aCallArgs,
-                  verb = aCall$verb,
+                  required = aCall[["required"]],
+                  verb = aCall[["verb"]],
                   version = brapiSpecs[["info"]][["version"]],
-                  tag = aCall$tags)
+                  tag = aCall[["tags"]])
 
 template <- readLines(con = "inst/templates/function_name.mst")
 
