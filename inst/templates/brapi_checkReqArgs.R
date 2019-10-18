384cbb12c956f50c1fdb415a60f8f2a3d69b4db1
mverouden <maikel.verouden@gmail.com>
2019-10-18 18:23:58 +0200

    modified the brapi_checkReqArgs function

--- inst/templates/brapi_checkReqArgs.R
+++ inst/templates/brapi_checkReqArgs.R
@@ -3,6 +3,6 @@ brapi_checkReqArgs <- function(...) {
   args <- list(...)
   non_empty <- function(s) {nchar(s) > 0}
   if (!lapply(X = args, FUN = non_empty) %>% unlist %>% all()) {
-    stop("All required parameters must have at least a length of one.")
+    stop("All required arguments must have at least a length of one.")
   }
 }
