af67d862dd30131a2178498433a5fb9881185087
mverouden <maikel.verouden@gmail.com>
2019-10-25 15:50:49 +0200

    removed the brapi_checkReqArgs in favor of brapi_checkArg function, which checks used and required arguments in one function

--- inst/templates/brapi_checkReqArgs.R
+++
@@ -1,8 +0,0 @@
-### Internal function to check required arguments are filled
-brapi_checkReqArgs <- function(...) {
-  args <- list(...)
-  non_empty <- function(s) {nchar(s) > 0}
-  if (!lapply(X = args, FUN = non_empty) %>% unlist %>% all()) {
-    stop("All required arguments must have at least a length of one.")
-  }
-}
