fb3dcf49de1bae4f6271400d55a78f92d36591f4
mverouden <maikel.verouden@gmail.com>
2019-11-01 18:48:34 +0100

    new match.arg2 function to be able to match empty string in the choices argument, modification of match.arg

---
+++ inst/templates/match.arg2.R
@@ -0,0 +1,29 @@
+### Revision of match.arg to be able to match empty strings
+match.arg2 <- function(arg, choices, several.ok = FALSE) {
+  if (missing(choices)) {
+    formal.args <- formals(sys.function(sys.parent()))
+    choices <- eval(formal.args[[as.character(substitute(arg))]])
+  }
+  if (is.null(arg)) {
+    return(choices[1L])
+  } else {
+    if (!is.character(arg)) {stop("'arg' must be NULL or a character vector")}
+  }
+  if (!several.ok) {
+    if (identical(arg, choices)) {return(arg[1L])}
+    if (length(arg) > 1L) {stop("'arg' must be of length 1")}
+  } else {
+    if (length(arg) == 0L) {stop("'arg' must be of length >= 1")}
+  }
+  i <- charmatch(arg, choices, nomatch = 0L)
+  if (all(i == 0L)) {
+    stop(gettextf("'arg' should be one of %s",
+                  paste(dQuote(choices), collapse = ", ")),
+         domain = NA)
+  }
+  i <- i[i > 0L]
+  if (!several.ok && length(i) > 1) {
+    stop("there is more than one match in 'match.arg2'")
+  }
+  return(choices[i])
+}
