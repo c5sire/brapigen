a6c718c82c1f3c0a4bdf95eb48ab68f1eac56947
mverouden <maikel.verouden@gmail.com>
2019-11-06 14:07:13 +0100

    changed res into resp in brapi_GET function

--- inst/templates/brapi_GET.R
+++ inst/templates/brapi_GET.R
@@ -1,22 +1,22 @@
-## Internal function receiving a response for a GET call
+### Internal function to obtain a response from a GET call
 brapi_GET <- function(url, usedArgs) {
   brapi_message(msg = paste0("URL call was: ", url, "\n"))
   brapi_message(msg = paste0("Waiting for response from server: ...\n"))
 
   if ("Accept" %in% names(usedArgs)) {
-    res <- httr::GET(url = url,
+    resp <- httr::GET(url = url,
                      httr::timeout(25),
                      httr::add_headers("Accept" = paste(usedArgs[["Accept"]]),
                                        "Authorization" = paste("Bearer", usedArgs[["con"]][["token"]])))
   } else {
-    res <- httr::GET(url = url,
+    resp <- httr::GET(url = url,
                      httr::timeout(25),
                      httr::add_headers("Authorization" = paste("Bearer", usedArgs[["con"]][["token"]])))
   }
 
-  txt <- ifelse(res$status == 200, " ok!", " problem!")
+  txt <- ifelse(resp$status == 200, " ok!", " problem!")
   brapi_message(msg = paste0("Server status: ", txt, "\n"))
-  # url <- httr::content(res)
-  # if (format == "json") show_server_status_messages(res)
-  return(res)
+  # url <- httr::content(resp)
+  # if (format == "json") show_server_status_messages(resp)
+  return(resp)
 }
