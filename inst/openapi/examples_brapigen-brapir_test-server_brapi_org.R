e1016d8e8e6c78ca439dd2b06ae1374b9ca8662a
Verouden, Maikel <maikel.verouden@gmail.com>
2019-12-17 16:39:26 +0100

    tested get calls up to /studies/{studyDbId}/table

--- inst/openapi/examples_brapigen-brapir_test-server_brapi_org.R
+++ inst/openapi/examples_brapigen-brapir_test-server_brapi_org.R
@@ -1,4 +1,4 @@
-## test calls
+## test calls: CONTINUE WITH 18
 ## result section: Master
 brapi_get_images_imageDbId(con = con, imageDbId = "img1")
 brapi_get_methods_methodDbId(con = con, methodDbId = "m1")
@@ -11,18 +11,20 @@ brapi_get_germplasm_germplasmDbId(con = con, germplasmDbId = "3")
 brapi_get_calls(con = con)#, dataType = "application/json") # 1
 brapi_get_commoncropnames(con = con)#, pageSize = 1, page = 1) # 2
 brapi_get_programs(con = con)#, commonCropName = "Tomatillo", programName = "Program 1", abbreviation = "P1", pageSize = 1, page = 0) # 3
-brapi_get_search_programs_searchResultsDbId(con = con, searchResultsDbId = "")#, pageSize = 1000, page = 0) # 4 (returns empty json!)
+brapi_get_search_programs_searchResultsDbId(con = con, searchResultsDbId = "")#, pageSize = 1000, page = 0) # 4
 brapi_get_locations(con = con)#, locationType = "Storage location") # 5
 brapi_get_trials(con = con)#, commonCropName = "", programDbId = "", locationDbId = "", active = NA, sortBy = "", sortOrder = "", pageSize = "", page = "") # 7
-brapi_get_search_studies_searchResultsDbId(con = con, searchResultsDbId = "")#, pageSize = 1000, page = 0) # 9 (returns empty json!)
+brapi_get_search_studies_searchResultsDbId(con = con, searchResultsDbId = "")#, pageSize = 1000, page = 0) # 9
 brapi_get_seasons(con = con)#, seasonDbId = "", season = "", year = "", pageSize = 1000, page = 0) # 10
 brapi_get_studies(con = con)#, commonCropName = "", studyTypeDbId = "", programDbId = "", locationDbId = "", seasonDbId = "", trialDbId = "", studyDbId = "", active = NA, sortBy = "", sortOrder = "", pageSize = 1000, page = 0) # 11
-brapi_get_studies_studyDbId_layouts(con = con, studyDbId = "1001")#, pageSize = 1000, page = 0) # 14 # returns a code 500 internal server error
+brapi_get_studies_studyDbId_layouts(con = con, studyDbId = "1001")#, pageSize = 1000, page = 0) # 14
 brapi_get_studies_studyDbId_observations(con = con, studyDbId = "1001")# , observationVariableDbIds = '', page = 0, pageSize = 1000) # 15
-brapi_get_studies_studyDbId_observationunits(con = con, studyDbId = "1001")#, observationLevel = "", pageSize = 1000, page = 0) # 16 CONTINUE HERE: ERROR!
+brapi_get_studies_studyDbId_observationunits(con = con, studyDbId = "1001")#, observationLevel = "", pageSize = 1000, page = 0) # 16
+brapi_get_studies_studyDbId_table(con = con, studyDbId = "1001")#, format = "csv") # 18
 brapi_get_markers(con = con)
 
 
 ## result section: Master/Detail
 brapi_get_studies_studyDbId_germplasm(con = con, studyDbId = "1001")#, pageSize = 1000, page = 0) # 13
+brapi_get_studies_studyDbId_observationvariables(con = con, studyDbId = "1001")#, pageSize = 1000, page = 0) # 17
 brapi_get_germplasm_germplasmDbId_attributes(con = con, germplasmDbId = "3")
