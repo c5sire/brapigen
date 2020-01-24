7cc85ae01082752cc58e8e1188250e961b8cd89d
Verouden, Maikel <maikel.verouden@gmail.com>
2020-01-24 13:15:17 +0100

    tested all 72 GET call examples, two fixed to be implemented still!

--- inst/openapi/examples_brapigen-brapir_test-server_brapi_org.R
+++ inst/openapi/examples_brapigen-brapir_test-server_brapi_org.R
@@ -11,12 +11,17 @@ brapi_get_traits_traitDbId(con = con, traitDbId = "t1") # 31
 brapi_get_variables_observationVariableDbId(con = con, observationVariableDbId = "MO_123:100002") # 33
 brapi_get_breedingmethods_breedingMethodDbId(con = con, breedingMethodDbId = "bm1") # 35
 brapi_get_germplasm_germplasmDbId(con = con, germplasmDbId = "1") # 37
-brapi_get_markers_markerDbId(con = con, markerDbId = "mr01") # 40
-brapi_get_samples_sampleDbId(con = con, sampleDbId = "sam01") # 51
-brapi_get_vendor_orders_orderId_status(con = con, orderId = "vo1") # 56
-brapi_get_vendor_plates_submissionId(con = con, submissionId = "ps1") # 57 MANUALLY ADDED DESCRIPTION OF submissionId as: The submissionId returned by the vendor, when a set of plates was successfully submitted. It is obtained from the response of "POST /vendor/plates" call.
-brapi_get_vendor_specifications(con = con) # 58 MANUALLY ADAPTED function arguments! ERROR WITH OUTPUT!
-brapi_get_images_imageDbId(con = con, imageDbId = "img1") # 62
+brapi_get_germplasm_germplasmDbId_markerprofiles(con = con, germplasmDbId = "1") # 39
+brapi_get_germplasm_germplasmDbId_mcpd(con = con, germplasmDbId = "1") # 40
+brapi_get_germplasm_germplasmDbId_pedigree(con = con, germplasmDbId = "1")#, notation = "", includeSiblings = NA) # 41
+brapi_get_germplasm_germplasmDbId_progeny(con = con, germplasmDbId = "1") # 42
+brapi_get_markers_markerDbId(con = con, markerDbId = "mr01") # 47
+brapi_get_samples_sampleDbId(con = con, sampleDbId = "sam01") # 58
+brapi_get_vendor_orders_orderId_status(con = con, orderId = "vo1") # 63
+brapi_get_vendor_plates_submissionId(con = con, submissionId = "ps1") # 64 MANUALLY ADDED DESCRIPTION OF submissionId as: The submissionId returned by the vendor, when a set of plates was successfully submitted. It is obtained from the response of "POST /vendor/plates" call.
+brapi_get_vendor_specifications(con = con) # 65 MANUALLY ADAPTED function arguments! ERROR WITH OUTPUT!
+brapi_get_images_imageDbId(con = con, imageDbId = "img1") # 69
+brapi_get_people_personDbId(con = con, personDbId = "person1") # 72
 
 ## result section: Detail
 brapi_get_calls(con = con)#, dataType = "application/json") # 1
@@ -45,27 +50,31 @@ brapi_get_traits(con = con)#, page = 0, pageSize = 1000) # 30
 brapi_get_variables(con = con)#, observationVariableDbId = "", traitClass = "", page = 0, pageSize = 1000) # 32
 brapi_get_breedingmethods(con = con)#, page = 0, pageSize = 1000) # 34
 brapi_get_germplasm(con = con)#, germplasmPUI = "", germplasmDbId = "", germplasmName = "", commonCropName = "", page = 0, pageSize = 1000) # 36
-brapi_get_markers(con = con)#, markerDbId = "", markerName = "", includeSynonyms = NA, type = "", page = 0, pageSize = 1000) # 39
-brapi_get_search_markers_searchResultsDbId(con = con, searchResultsDbId = "")#, page = 0, pageSize = 1000) # 41
-brapi_get_allelematrices(con = con, studyDbId = "1001")#, page = 0, pageSize = 1000) # 42
-brapi_get_allelematrices_search(con = con)#, markerProfileDbId = c(""), markerDbId = c(""), matrixDbId = c(""), format = "", expandHomozygotes = NA, unknownString = "", sepPhased = "", sepUnphased = "", page = 0, pageSize = 1000) # 43 Changed to _search!
-brapi_get_markerprofiles(con = con)#, germplasmDbId = "", studyDbId = "", sampleDbId = "", extractDbId = "", page = 0, pageSize = 1000) # 44
-brapi_get_maps(con = con)#, commonCropName = "", scientificName = "", type = , page = 0, pageSize = 1000) # 46
-brapi_get_maps_mapDbId_positions(con = con, mapDbId = "gm2")#, linkageGroupName = "", page = 0, pageSize = 1000) # 48
-brapi_get_maps_mapDbId_positions_linkageGroupName(con = con, mapDbId = "gm2", linkageGroupName = "3")#, min = , max = , page = 0, pageSize = 1000) # 49
-brapi_get_samples(con = con)#, sampleDbId = "", observationUnitDbId = "", plateDbId = "", germplasmDbId = "", page = 0, pageSize = 1000) # 50
-brapi_get_search_samples_searchResultsDbId(con = con, searchResultsDbId = "")#, page = 0, pageSize = 4) # 52
-brapi_get_vendor_orders(con = con)#, orderId = ""), submissionId = "") # 53
-brapi_get_vendor_orders_orderId_plates(con = con, orderId = "vo1") # 54
-brapi_get_vendor_orders_orderId_results(con = con, orderId = "vo1") # 55
-brapi_get_lists(con = con)#, listType = "", listName = "", listDbId = "", listSource = "", page = 0, pageSize = 1000) # 59
-brapi_get_images(con = con)#, imageDbId = "", imageName = "", observationUnitDbId = "", observationDbId = "", descriptiveOntologyTerm = "", page = 0, pageSize = 1000) # 61
-brapi_get_search_images_searchResultsDbId(con = con, searchResultsDbId = "")#, page = 0, pageSize = 1000) # 63 Empty POST call!
+brapi_get_search_germplasm_searchResultsDbId(con = con, searchResultsDbId = "")#, page = 0, pageSize = 1000) # 43
+brapi_get_attributes(con = con)#, attributeCategoryDbId = "", page = 0, pageSize = 1000) # 44
+brapi_get_attributes_categories(con = con, page = 0)#, pageSize = 1000) # 45
+brapi_get_markers(con = con)#, markerDbId = "", markerName = "", includeSynonyms = NA, type = "", page = 0, pageSize = 1000) # 46
+brapi_get_search_markers_searchResultsDbId(con = con, searchResultsDbId = "")#, page = 0, pageSize = 1000) # 48
+brapi_get_allelematrices(con = con, studyDbId = "1001")#, page = 0, pageSize = 1000) # 49
+brapi_get_allelematrices_search(con = con)#, markerProfileDbId = c(""), markerDbId = c(""), matrixDbId = c(""), format = "", expandHomozygotes = NA, unknownString = "", sepPhased = "", sepUnphased = "", page = 0, pageSize = 1000) # 50 Changed to _search!
+brapi_get_markerprofiles(con = con)#, germplasmDbId = "", studyDbId = "", sampleDbId = "", extractDbId = "", page = 0, pageSize = 1000) # 51
+brapi_get_maps(con = con)#, commonCropName = "", scientificName = "", type = , page = 0, pageSize = 1000) # 53
+brapi_get_maps_mapDbId_positions(con = con, mapDbId = "gm2")#, linkageGroupName = "", page = 0, pageSize = 1000) # 55
+brapi_get_maps_mapDbId_positions_linkageGroupName(con = con, mapDbId = "gm2", linkageGroupName = "3")#, min = , max = , page = 0, pageSize = 1000) # 56
+brapi_get_samples(con = con)#, sampleDbId = "", observationUnitDbId = "", plateDbId = "", germplasmDbId = "", page = 0, pageSize = 1000) # 57
+brapi_get_search_samples_searchResultsDbId(con = con, searchResultsDbId = "")#, page = 0, pageSize = 4) # 59
+brapi_get_vendor_orders(con = con)#, orderId = ""), submissionId = "") # 60
+brapi_get_vendor_orders_orderId_plates(con = con, orderId = "vo1") # 61
+brapi_get_vendor_orders_orderId_results(con = con, orderId = "vo1") # 62
+brapi_get_lists(con = con)#, listType = "", listName = "", listDbId = "", listSource = "", page = 0, pageSize = 1000) # 66
+brapi_get_images(con = con)#, imageDbId = "", imageName = "", observationUnitDbId = "", observationDbId = "", descriptiveOntologyTerm = "", page = 0, pageSize = 1000) # 68
+brapi_get_search_images_searchResultsDbId(con = con, searchResultsDbId = "")#, page = 0, pageSize = 1000) # 70 Empty POST call!
+brapi_get_people(con = con)#, firstName = "", lastName = "", personDbId = "", userID = "", page = 0, pageSize = 1000) # 71
 
 ## result section: Master/Detail
 brapi_get_studies_studyDbId_germplasm(con = con, studyDbId = "1001")#, pageSize = 1000, page = 0) # 13
 brapi_get_studies_studyDbId_observationvariables(con = con, studyDbId = "1001")#, pageSize = 1000, page = 0) # 17
-brapi_get_germplasm_germplasmDbId_attributes(con = con, germplasmDbId = "1")#, attributeDbIds = c("ATT01", "ATT05"), page = 0, pageSize = 1000) # 38
-brapi_get_markerprofiles_markerProfileDbId(con = con, markerProfileDbId = "P1")#, expandHomozygotes = NA, unknownString = "", sepPhased = "", sepUnphased = "", page = 0, pageSize = 1000) # 45
-brapi_get_maps_mapDbId(con = con, mapDbId = "gm2")#, page = 0, pageSize = 1000) # 47
-brapi_get_lists_listDbId(con = con, listDbId = "list1") # 60
+brapi_get_germplasm_germplasmDbId_attributes(con = con, germplasmDbId = "1")#, attributeDbIds = c("", ""), page = 0, pageSize = 1000) # 38
+brapi_get_markerprofiles_markerProfileDbId(con = con, markerProfileDbId = "P1")#, expandHomozygotes = NA, unknownString = "", sepPhased = "", sepUnphased = "", page = 0, pageSize = 1000) # 52
+brapi_get_maps_mapDbId(con = con, mapDbId = "gm2")#, page = 0, pageSize = 1000) # 54
+brapi_get_lists_listDbId(con = con, listDbId = "list1") # 67
