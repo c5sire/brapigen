e9c81477a5b0ccf9839c2df45f238fa6d067d460
Verouden, Maikel <maikel.verouden@gmail.com>
2020-02-05 12:18:05 +0530

    added scripts to create dereferenced yaml files for package generation including a txt file how this was done

---
+++ inst/openapi/derefAll2Filev2.0.py
@@ -0,0 +1,83 @@
+
+import yaml
+import sys
+
+def dereferenceAll(obj, parent):
+    #print(breadCrumb)
+    try:
+        if type(obj) is dict:
+            for fieldStr in obj:
+                #print(fieldStr)
+                if(fieldStr == '$ref'):
+                    refPath = obj[fieldStr].split('/')[1:]
+                    refObj = parent
+                    for refPart in refPath:
+                        if refPart in refObj:
+                            refObj = refObj[refPart]
+                        else:
+                            raise Exception('Schema not found ' + obj[fieldStr])
+
+                    refObj = dereferenceAll(refObj, parent)
+                    #refObj['title'] = refPath[-1]
+                    obj = {**obj, **refObj}
+                elif(fieldStr == 'allOf'):
+                    comboObj = {'properties': {}, 'type': 'object'}
+                    for item in obj[fieldStr]:
+                        itemObj = dereferenceAll(item, parent)
+                        comboObj['properties'] = {**(comboObj['properties']), **(itemObj['properties'])}
+                        if 'title' in itemObj:
+                            comboObj['title'] = itemObj['title']
+                        if 'description' in itemObj:
+                            comboObj['description'] = itemObj['description']
+                        if 'example' in itemObj:
+                            comboObj['example'] = itemObj['example']
+                        
+                    obj = comboObj
+                else:
+                    obj[fieldStr] = dereferenceAll(obj[fieldStr], parent)
+            if '$ref' in obj:
+                obj.pop('$ref')
+        elif type(obj) is list:
+            newList = []
+            for item in obj:
+                newList.append(dereferenceAll(item, parent))
+            obj = newList
+    except Exception as ex:
+        ##print(obj)
+        raise ex
+    return obj
+
+
+def dereferenceBrAPI(filePath = './brapi_openapi.yaml', verbose = False):    
+    fileObj = {}
+    if verbose :
+        print(filePath)
+    with open(filePath, "r") as stream:
+        try:
+            fileObj = yaml.load(stream)
+            stream.close()
+        except yaml.YAMLError as exc:
+            stream.close()
+            if verbose :
+                print(exc)
+    
+    fileObj = dereferenceAll(fileObj, fileObj)
+    return fileObj;
+
+def str_presenter(dumper, data):
+  if len(data.splitlines()) > 1:  # check for multiline string
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+  return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+yaml.add_representer(str, str_presenter)
+noalias_dumper = yaml.dumper.SafeDumper
+noalias_dumper.ignore_aliases = lambda self, data: True
+if len(sys.argv) > 2 :
+    inFilePath = sys.argv[1]
+    outFilePath = sys.argv[2]
+    print(inFilePath)
+    print(outFilePath)
+    out = dereferenceBrAPI(inFilePath)
+    out.pop('components')
+    with open(outFilePath, 'w') as outfile:
+        yaml.dump(out, outfile, default_flow_style=False, width=float("inf"), Dumper=noalias_dumper)
